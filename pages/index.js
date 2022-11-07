f51342a446cc3655fd500d909872c55959429cdb
Henrique Saias <henriquesaias@hsaias.com>
2022-11-07 21:31:57 +0000

    first couple of pages

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import { useRouter } from 'next/router'
-import { Button, Image } from 'semantic-ui-react'
+import { Button, Grid, Image } from 'semantic-ui-react'
 
 export default function Home() {
   const router = useRouter()
@@ -10,7 +10,7 @@ export default function Home() {
       <Head>
         <title>Janax Pacha</title>
         <meta name="description" content="The Land of Heaven" />
-        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -21,17 +21,52 @@ export default function Home() {
             <Image ui={false} src="/logo pacha.png" className='ml1' />
           </div>
           <div id="splash-nav">
-            <Button>Listen</Button>
-            <Button>Watch</Button>
-            <Button>Experience</Button>
+            <Button onClick={() => router.push('#listen')}>Listen</Button>
+            <Button onClick={() => router.push('#watch')}>Watch</Button>
+            <Button onClick={() => router.push('#experience')}>Experience</Button>
           </div>
           <div id="splash-arrows">
             <Image src="/down arrow.png" />
             <Image src="/down arrow.png" />
           </div>
         </section>
-        <section>
-          
+
+        <section id="quote-container">
+          <div id="hanaq-quote">
+            <p>In the Native tribes from the Andes, <br />the Quechua people call “Hanaq Pacha” the Land of Heaven.</p>
+          </div>
+        </section>
+
+        <section className='hidden-mobile'>
+          <Image src='/home pic cropped.png' />
+        </section>
+
+        <section id="about-me">
+          <Grid padded='horizontally'>
+            <Grid.Row>
+              <Grid.Column mobile={0} computer={8}></Grid.Column>
+              <Grid.Column mobile={16} computer={7}>
+                <h2>About me</h2>
+
+                <div className='icons-container'>
+                  <Image src='/dot.png' /> <Image src='/dot2.png' /> <Image src='/dot3.png' /> <Image src='/arrow right.png' /> <Image src='/arrow right.png' /> <Image src='/arrow right.png' /> <Image src='/dot4.png' /> <Image src='/dot5.png' /> <Image src='/dot6.png' />
+                </div>
+
+                <div id="about-text">
+                  <p>I am Gabriel Epstein, born in Argentina.</p>
+                  <p>I am a multi-instrumentalist music producer focused on the fusion of electronic music with ethnic and tribal instruments.</p>
+                  <p>My travels through different cultures and the connection with shamanism is fully present and is the essence of my music.</p>
+                </div>
+
+                <div id='about-final-quote'>
+                  <p>I invite you to discover<br />the beauty of your true Nature.</p>
+                  <p>Let sound & vibration take you on this journey..</p>
+                </div>
+
+                <Image src="/down triangles.png" centered />
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
         </section>
       </main>
     </div>
