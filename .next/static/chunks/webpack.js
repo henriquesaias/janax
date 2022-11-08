1242872749d4e65190f96287bcabd5724316b36f
Henrique Saias <henriquesaias@hsaias.com>
2022-11-08 02:11:27 +0000

    fix images in css

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1a45256437ff7b57"; }
+/******/ 		__webpack_require__.h = function() { return "7e18124498073779"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
