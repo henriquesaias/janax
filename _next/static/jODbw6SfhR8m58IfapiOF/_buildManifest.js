f5a84ce9e36e28c0859cbc36c2213408c6d7efd9
henriquesaias <31808055+henriquesaias@users.noreply.github.com>
2022-11-08 01:46:18 +0000

    Deploying to gh-pages from @ henriquesaias/janax@86d51f49d6f60a8331bba447f7923c4341caa2fc 🚀

--- _next/static/jODbw6SfhR8m58IfapiOF/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/713-2cfa5545cf1ac9ee.js","static/chunks/pages/index-fd1a019619a55bc7.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/pc0WBJaQheGoeGaNx36PN/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/713-2cfa5545cf1ac9ee.js","static/chunks/pages/index-fc2c07e0b5712cf9.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
