6179339de0434fd476549690852790dc829c86b1
Henrique Saias <henriquesaias@hsaias.com>
2022-11-08 01:55:28 +0000

    reset

---
+++ out/_next/static/qRzsD-kraWNLtNO2uj8FE/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/713-2cfa5545cf1ac9ee.js","static/chunks/pages/index-a33d30b8fd9cfd73.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
