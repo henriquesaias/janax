89e0d31389e8d1f32cf04b6c2d375f9daa4c4b84
henriquesaias <31808055+henriquesaias@users.noreply.github.com>
2022-11-15 01:29:11 +0000

    Deploying to gh-pages from @ henriquesaias/janax@70ccd1e3619812a0613988b420dd8e1e66fd190c 🚀

--- _next/static/GWdUxlcmZMPGi-kfLGil1/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/a79515f9-2c6f72d3aa26bc3f.js","static/chunks/980-1e75d81b68af825d.js","static/chunks/pages/index-e65cea8afd14b10c.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
