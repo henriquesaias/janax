a1ce1d8b0fdeb45e479a1650933c00c37762d458
Henrique Saias <henriquesaias@hsaias.com>
2022-11-28 11:26:50 +0000

    remove animation from quote section

--- components/HanaqQuote.jsx
+++ components/HanaqQuote.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export default function HanaqQuote({ section }) {
+export default function HanaqQuote() {
   return (
     <>
       <section id="quote-container">
-        <div id="hanaq-quote" className={section === 'quote' ? 'animate-into-view' : 'animate-out-of-view'}>
+        <div id="hanaq-quote">
           <p>In the Native tribes from the Andes, <br />the Quechua people call “Hanaq Pacha” the Land of Heaven.</p>
         </div>
       </section>
