ecbfabac3934c90a117da2ff71b091278e0634d3
Henrique Saias <henriquesaias@hsaias.com>
2022-11-29 14:37:45 +0000

    remove animation from arrows

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2a025188138dda6f"; }
+/******/ 		__webpack_require__.h = function() { return "37f7d38f5046a050"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
