559df3fcb98f99bbbdc040319a9d23c7d17910b2
Henrique Saias <henriquesaias@hsaias.com>
2023-03-21 18:38:09 +0000

    revert yml, trying with next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   reactStrictMode: true,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
+  assetPrefix: isProd ? '' : '/janax/'
 }
 
 module.exports = nextConfig
