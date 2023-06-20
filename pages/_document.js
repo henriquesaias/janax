a90f300bf129646ad13babd13fb1c4b9b9781013
henriquesaias <31808055+henriquesaias@users.noreply.github.com>
2023-06-20 14:56:01 +0100

    Add google script to _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,6 @@
 
 import { Html, Head, Main, NextScript } from 'next/document'
+import { Script } from 'next/head'
 
 export default function Document() {
   return (
@@ -8,6 +9,12 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=Averia+Sans+Libre:ital@0;1&family=Cabin+Sketch&family=Neucha&family=Roboto:wght@300;400&display=swap" rel="stylesheet" />
+        <Script 
+          async 
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7127008256948874"
+          crossorigin="anonymous" 
+          onError={ (e) => { console.error('Script failed to load', e) }}
+        />
       </Head>
       <body>
         <Main />
